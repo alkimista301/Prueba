a4d2a3076da402e8406e16a440cddf455ef8ef86
edwards <edwardssmithp053@gmail.com>
2025-11-20 20:07:41 -0500

    mi primer commit

---
+++ pruebav1/Models/Usuario.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace pruebav1.Models
+{
+    public class Usuario
+    {
+
+        public string Id { get; set; }
+
+        [Required(ErrorMessage ="es obligatorio agregar tu nombre")]
+        [StringLength(100,ErrorMessage ="solo ingresar 100 caracteres")]
+        public string Name { get; set; }
+
+    }
+}
