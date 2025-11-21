db9b89f29672ac54ac9d27895d0bbf1b4a5adf41
milagros <munozcm.1989@gmail.com>
2025-11-20 21:06:04 -0500

    Agregué cambios en Register.cshtml y Usuario.cs

--- pruebav1/Models/Usuario.cs
+++ pruebav1/Models/Usuario.cs
@@ -11,5 +11,9 @@ namespace pruebav1.Models
         [StringLength(100,ErrorMessage ="solo ingresar 100 caracteres")]
         public string Name { get; set; }
 
+        public decimal Price { get; set; }  
+
+        public string Description { get; set; }
+
     }
 }
