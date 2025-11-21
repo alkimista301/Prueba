eacfeffe392ae275db5d5868aa76bab1a7344bdc
edwards <edwardssmithp053@gmail.com>
2025-11-20 21:18:34 -0500

    realice el proceso del usuariocontroller

---
+++ pruebav1/Controllers/UsuarioController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using pruebav1.Models;
+
+namespace pruebav1.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        private IActionResult ObtenerUsuario(Usuario usuario)
+        {
+            return View(usuario);
+        }
+    }
+}
